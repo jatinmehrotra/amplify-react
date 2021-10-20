dcc3342bf42aa42bd935ae208ac45245deea40e4
Jatin Mehrotra <jatinmehrotra@HL00796.local>
2021-10-20 15:15:27 +0530

    shawarma message added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I love coffee!</p>
+        <p>I love coffee and shawarma</p>
         <a
           className="App-link"
           href="https://reactjs.org"
