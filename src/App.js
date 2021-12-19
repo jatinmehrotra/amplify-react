9346380db08fac6a552cf51b0249633d4808bf2a
Jatin Mehrotra <jatinmehrotra@HL00796.local>
2021-12-19 17:07:10 +0530

    amplify message test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I love coffee and shawarma</p>
+        <p>I love coffee, shawarma</p>
         <a
           className="App-link"
           href="https://reactjs.org"
